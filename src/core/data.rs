b2706a416c85aac7ee592b3cf8c1ff5190a9d726
Ruben Sosenke <rsosenke@gmail.com>
2024-08-11 15:30:26 -0300

    Added Series list in application memory

--- src/core/data.rs
+++ src/core/data.rs
@@ -4,8 +4,10 @@ use std::collections::HashMap;
 use std::sync::{Arc, Mutex};
 
 lazy_static! {
-    // This is a HashMap that stores the machines in the system.
+    // HashMap that stores the machines in the system.
     pub static ref MACHINES: Arc<Mutex<HashMap<String, Machine>>> = Arc::new(Mutex::new(HashMap::new()));
-    // This is a Vec that stores the names of manufacturers.
+    // Vec that stores the names of manufacturers.
     pub static ref MANUFACTURERS: Arc<Mutex<Vec<String>>> = Arc::new(Mutex::new(Vec::new()));
+    // Vec that stores the names of the series.
+    pub static ref SERIES: Arc<Mutex<Vec<String>>> = Arc::new(Mutex::new(Vec::new()));
 }
